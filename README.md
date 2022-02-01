3056d11c8ef4e828f5945d093fdd0a2ea9cba9f2
Rafael Maciel <projetos.swr@gmail.com>
2022-02-01 16:25:01 -0300

    fouth commit - full code and full comments

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">
+  <img alt="" title="typescript fundamentals" src=".github/demonstration_aplication.png" width="120px" />
+  <br>
+  RocketShoes - Optimized Shopping Cart 
+</h1>
+
+
+<h3 align="justify">
+Didactic application of an optimized shopping cart using JS and the TypeScript super set. Used concepts of Fake API with JSON Server, Prediction of cart data with localStorage API, and error indication with toastify. Componentization and hook concepts were used. The functionalities obtained in the challenge are adding a new product to the cart, removing a product from the cart, changing the quantity of a product in the cart, calculating the sub-total and total prices of the cart, validating stock and displaying messages of mistake. Credits for RocketSeat's ReactJS track Chapter 2 challenge.
+</h3>
+<br>
+
+## 📷 Demonstration
+
+<div align="center" >
+<h4 align="left"></h4>
+  <img src=".github/demostration_aplication_1.gif">
+</div>
+<br>
+
+## 🚀 Technologies
+
+This project was developed with the following technologies:
+
+- [TypeScript](https://www.typescriptlang.org/docs/)
+- [React](https://reactjs.org/)
+- [JSONServer](https://github.com/typicode/json-server)
+- [Axios](https://axios-http.com/docs/intro)
+
+<br>
+
+## ⚙ Commands Running locally Client
+- yarn start
+
+## ⚙ Commands Running locally Server
+- yarn server
+
+## ⚙ Documentatios
+- <a href=".github/Documentation.pdf">PDF</a>
+- [RocketSeat](https://www.notion.so/Desafio-01-Criando-um-hook-de-carrinho-de-compras-5769216778794019a83f544e79167b12)
+<br>
+
+Made with 💜 by Rafael Maciel
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product } from '../types';
@@ -33,6 +33,27 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+// Bonus: Tem uma forma da gente resolver aquilo do useCart, sem ter que dar setItem toda vez aqui no final. Essa solução veio da documentação do React. Site do React, documentação, hooks, hooks faq, de classes de hook, como acessar as propos ou o estado anterior. Então, como acessar as props ou o estado anterior? Então ele utiliza uma ref,  pra gente acessar o estado anterior, o valor anterior do estado. Por que isso? Ora, a ideia é em vez de ficar setando sempre no final das funções, eu vou ficar verificando se quando o meu hook renderizar novamente, porque agente sabe que os componentes do react ficam renderizando, eu vou verificar se o alor atual do carrinho é diferente do valor anterior. Se for diferente, isso significa que ele atualizou. Então eu vou ter que alterar esse valor; e alterar esse valor onde? no localStorage. 
+// Primeira coisa que eu quero fazer, é criar uma Ref. Vou chama-la de prevCartRef. Depois eu tenho meu useRef. No meu useRef, vou passar a tipagem dele como o do carrinho, pois assim ele pode passar por generic. Depois vou fazer um useEffect, pois ele vai rodar todas as vezes que isso aqui renderizar novamente. Então toda a vez que renderizar novamente, eu quero que o current reebe o valor de cart. 
+  const prevCartRef = useRef<Product[]>();
+
+  // Perceba que para o meu useEffect rodar todas as vezes que esse cart provider renderizar novamente, eu não faço nada aqui no meu array de dependencias. Eu passo ele simplesmente dessa forma aqui.  
+// Agora vou fazer o seguinte, como agente não pode ficar monitorando uma referencia, pra ver se os valores dela atualizaram, não funciona voce colcoar o prevCartRef.current, por exemplo, no array de depencias do useEffect, eu vou pegar aqui uma variavel, cartPreviousValue, e ela vai receber o seguinte; ela vai receber o prevCartRef.current, se ele existir; porque perceba que ele pode ser um product, ou pode ser undefined. Então, na primeira vez que ele rodar o useEffect, ele vai ta undefined, e eu não quero que isso aconteça; porque quando eu for fazer a verificação,  se o valor cartPreviusValue é diferente do atual (CartProvider), ele vai entender que  é e vai atualizar; e vai dar um setItem; e não queremos isso. 
+  useEffect(() => {
+    prevCartRef.current = cart;
+  })
+
+  // Então o que vou fazer, vou usar o ?? (operator), então o que ele vai fazer, se o valor da esquerda for false, false, null ou undefined, ele vai atribuir o valor da direita (cart). Se o valor da esquerda NÃO for um valor falso, null ou undefined. Então na primeira passagem que o prevCartRef.current foi undefined, eu vou atribuir o valor do Cart; e eles vão estar iguais na hora que eu fizer meu if, ja na segunda passagem, ai ja vou atribuir o prevCartRef.current, ai ja não vai mais ser undefined, vai ser só o valor anterior; ai vai funcionar exatamente como esperamos. 
+  const cartPreviousValue = prevCartRef.current ?? cart;
+
+  // Agora vou rodar o useEffect, para finalmente fazer a lógica. Então if, cartPreviusValue (valor anterior do carrinho), for diferente (!==) do valor atual do carrinho (cart), significa que teve uma alteração no carrinho; então tenho que dar um setItem. Além disso eu recordo de dentro do const updateProductAmount, o localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)), pois não vamos mais usar ele la dentro. Colo ele em meu if e mudo a passagem do JSON.stringify de updateCart, para cart. Resumindo, se eles forem diferentes, eles atualizam conforme setItem abaixo do useEffect, se for igual, ou seja, não teve atualização, não tem que fazer nada. Agora perceba que nosso Array de depencencias ja esta acusando que temos que colocar as propriedades cart e cartPreviusValue, para o useEffect monitorar certinho.
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+    }
+  }, [cart, cartPreviousValue]);
+  //Em suma, a logica toda do Bonus trata-se de verificar o valor anterior do carrinho, comparar com a atual e se mudou eu dou setItem. 
+
   const addProduct = async (productId: number) => {
     try {
       const updatedCart = [...cart]; //Novo array a partir do que tenho no carrinho. Dessa forma, mantenho a imutabilidade e consigo trabalhar com os recursos do JS. UpdatedCart é um novo array com os valores de cart. Qualquer alteração que se fazer nele, não vai se refletir no cart.
@@ -66,7 +87,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
         setCart(updatedCart); // Para finalizar, tenho que dar um setCart com o updatedCart, para perpetuar essas alterações do updatedCart dentro do meu carrinho.
 
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) // E por fim, dar um localStorage.setItem. Eu tenho que passar o item do localStorage aqui. A chave tem que ser essa passado no exercicio para essa aplicação!. Se você perceber, o value do setItem deverá ser uma string. O valor naturalmente deveria ser o "updateCart", porém o mesmo é um array de produto. Logo não aceito. Assim, iremos transformar em string, com o JSON.stringify, passando o "updateCart" no parenteses.  
+        // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) // E por fim, dar um localStorage.setItem. Eu tenho que passar o item do localStorage aqui. A chave tem que ser essa passado no exercicio para essa aplicação!. Se você perceber, o value do setItem deverá ser uma string. O valor naturalmente deveria ser o "updateCart", porém o mesmo é um array de produto. Logo não aceito. Assim, iremos transformar em string, com o JSON.stringify, passando o "updateCart" no parenteses.  - Comentado conforme explicação do bonus. O useEffect la em cima vai aplciar o set Item sempre que necessário. 
 
     } catch { // Uma cláusula catch contém declarações que especificam o que fazer caso uma exceção seja lançada no bloco try
       toast.error('Erro na adição do produto'); //Mensagem fornecida pelo exercicio para aparecer caso der erro na adição do produto.
@@ -83,7 +104,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         updatedCart.splice(productIndex, 1); // Se ele encontrou, eu vou pegar do meu array. Depois vamos usar o Splice, que tem o poder de remover os elementos do array e se for necessário adcionar novos elementos ao array. Como parametros, dentro dos parenteses, eu passo o inicio (@param start) aonde eu quero começar a deletar e a quantidade (@param deleteCount) de itens que eu quero deletar. Nesse caso quero apenas deletar. Sendo assim, eu vou iniciar no productIndex; querendo deletar apenas 1 produto.
         setCart(updatedCart);
         // Para perpetuar as informações do carrinho atualizado no localStorage. Conforme abaixo.
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) 
+        // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))  Comentado conforme explicação do bonus. O useEffect la em cima vai apliciar o set Item sempre que necessário. 
       } else { 
       // Se ele não encontrar o item no carrinho? Como eu ja tenho uma mensagem de erro no Catch, no else posso forçar ele a dar erro, através do throw Error. Quando da o throw error, ele para de rodar no try e cai diretamente no catch, dando a mensagem abaixo.
         throw Error(); 
@@ -96,7 +117,8 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const updateProductAmount = async ({
     productId,
     amount, // Por padrão do programador, é ideal de receber esse "amount" considerando  que o valor ja é o valor desejado que eu quero do produto.
-  }: UpdateProductAmount) => { // Vamos perpetuar o valor no carrinho, sair instantaneamente da função se o quantidade do produto for maior ou igual a zero (então não posso deixar zerar um produto no carrinho, seria igual a ter um produto com quantidade negativa), mostrar um erro com a mensagem definida quando a quantidade do produto estourar em relação ao quantidade do estoque e no catch, mostrar a mensagem definido no exercicio pra dizer que ocorreu um erro na alteração da quantidade do produto; um erro geral, por assim dizer.        // Utilizamos o "updateproductAmount", porque se virmos a pagina Cart, index.tsx, na que eu chamar na "handleProductIncrement" e "handleProductDecrement", eu ja passo ele com git add
+  }: UpdateProductAmount) => { // Vamos perpetuar o valor no carrinho, sair instantaneamente da função se o quantidade do produto for maior ou igual a zero (então não posso deixar zerar um produto no carrinho, seria igual a ter um produto com quantidade negativa), mostrar um erro com a mensagem definida quando a quantidade do produto estourar em relação ao quantidade do estoque e no catch, mostrar a mensagem definido no exercicio pra dizer que ocorreu um erro na alteração da quantidade do produto; um erro geral, por assim dizer.        
+    // Utilizamos o "updateproductAmount", porque se virmos a pagina Cart, index.tsx, na que eu chamar na "handleProductIncrement" e "handleProductDecrement", eu ja passo ele com git add
 
     try {
       if (amount <= 0) { // Sendo assim, ja vou verificar se a quantidade desejada do produto for menor ou igual a zero, eu tenho que sair; sem alterar nada.
@@ -116,8 +138,8 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       if (productExists) { // Agora eu ja posso verificar se o produto existe.
         productExists.amount = amount; // Se o produto existe, productExists.amount recebe amount.
-        setCart(updatedCart); // Depois tenho que perpetuar o updateCart no carrinho.
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) // E dar o setItem com o valor atualizado.
+        setCart(updatedCart);// Depois tenho que perpetuar o updateCart no carrinho. 
+        // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) // E dar o setItem com o valor atualizado.  Comentado conforme explicação do bonus. O useEffect la em cima vai aplciar o set Item sempre que necessário. 
       } else { // Agora se ele não encontrar esse produto no carrinho, ai agente tem um problema. Como eu quero incrementar um produto no carrinho, sendo que ele nem existe? Porque aquele "addproduct" eu estou cliclando em um produto la na pagina home, que ele pode estar com zero, então faz sentido ele não existir; e se ele não existir eu adiciono esse produto. Porém, essa função aqui é somente para o update, então nao faz sentido ele não existir no carrinho, ele tem que existir. Então se ele não existe, agente da um throw "error", pra ele mostrar a menssagem geral do catch "Erro na alteração de quantidade do produto".
         throw Error();
       }
